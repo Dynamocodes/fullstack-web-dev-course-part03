dc1d8e4edf714bbfe93ffb1e046b0e14284a277a
dynamo <hietanenelias@gmail.com>
2022-09-20 13:50:12 +0300

    exercise 3.7

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
@@ -33,6 +34,7 @@ const generateId = () => {
 }
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 app.post('/api/persons', (request, response) => {
   const person = request.body
