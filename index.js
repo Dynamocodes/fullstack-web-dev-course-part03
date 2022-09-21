408061d91dcdef88b793d1449e2b6e03a1f5cfb1
dynamo <hietanenelias@gmail.com>
2022-09-21 23:23:24 +0300

    exercise 3.19

--- index.js
+++ index.js
@@ -15,32 +15,29 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
 
-const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } 
-    next(error)
-}
-
-app.use(errorHandler)
-
-app.post('/api/persons', (request, response) => {
-  let person = request.body
-  if(person.name === "" || person.number === ""){
-    response.status(400).json({"error": "name or number cannot be empty"})
-  }else{
-    let newPerson = new Person({
-        name: person.name,
-        number: person.number
-    })
-    newPerson.save().then(result => {
-        console.log(`added ${newPerson} to the phonebook`)
-        response.json(newPerson)
-    })
-  }
+/* adding a new entry to the phonebook */
+app.post('/api/persons', (request, response, next) => {
+    let person = request.body
+    if(person.name === "" || person.number === ""){
+        response.status(400).json({"error": "name or number cannot be empty"})
+    }else{
+        let newPerson = new Person({
+            name: person.name,
+            number: person.number
+        })
+        newPerson
+        .save()
+        .then(result => {
+            console.log(`added ${newPerson} to the phonebook`)
+            response.json(newPerson)
+        })
+        .catch(error => {
+            next(error)
+        })
+    }
 })
 
+/* updating an existing entry to the phonebook */
 app.put('/api/persons/:id', (request, response, next) => {
     const body = request.body
   
@@ -48,13 +45,17 @@ app.put('/api/persons/:id', (request, response, next) => {
       content: body.content,
       important: body.important,
     }
-  
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
-      .then(updatedPerson => {
+    Person
+    .findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query'})
+    .then(updatedPerson => {
         response.json(updatedPerson)
-      })
-      .catch(error => next(error))
-  })
+    })
+    .catch(error => {
+        next(error)
+    })
+    
+})
+
 /* root route */
 app.get('/', (request, response, next) => {
   response.send('<h1>Hello World!</h1>')
@@ -103,8 +104,22 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+    next(error)
+}
+
+app.use(errorHandler)
+
 /* defining port at which the app will be run */
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
