0c5df7c3301445b1f4b0a31967674818b82aea49
dynamo <hietanenelias@gmail.com>
2022-09-22 13:29:03 +0300

    exercise 3.21-3.22

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const Person = require('./models/person')
 
 const app = express()
 
-morgan.token('body', function(req, res) {
-    return JSON.stringify(req.body);
-});
+morgan.token('body', function(req) {
+  return JSON.stringify(req.body)
+})
 
 app.use(express.static('build'))
 app.use(express.json())
@@ -17,105 +17,105 @@ app.use(cors())
 
 /* adding a new entry to the phonebook */
 app.post('/api/persons', (request, response, next) => {
-    let person = request.body
-    if(person.name === "" || person.number === ""){
-        response.status(400).json({"error": "name or number cannot be empty"})
-    }else{
-        let newPerson = new Person({
-            name: person.name,
-            number: person.number
-        })
-        newPerson
-        .save()
-        .then(result => {
-            console.log(`added ${newPerson} to the phonebook`)
-            response.json(newPerson)
-        })
-        .catch(error => {
-            next(error)
-        })
-    }
+  let person = request.body
+  if(person.name === '' || person.number === ''){
+    response.status(400).json({ 'error': 'name or number cannot be empty' })
+  }else{
+    let newPerson = new Person({
+      name: person.name,
+      number: person.number
+    })
+    newPerson
+      .save()
+      .then(() => {
+        console.log(`added ${newPerson} to the phonebook`)
+        response.json(newPerson)
+      })
+      .catch(error => {
+        next(error)
+      })
+  }
 })
 
 /* updating an existing entry to the phonebook */
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
-    const person = {
-      name: body.name,
-      number: body.number,
-    }
-    Person
-    .findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query'})
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+  Person
+    .findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
-        if(updatedPerson){
-            response.json(updatedPerson)
-        }else{
-            response.status(404).end()
-        }
+      if(updatedPerson){
+        response.json(updatedPerson)
+      }else{
+        response.status(404).end()
+      }
     })
     .catch(error => {
-        next(error)
+      next(error)
     })
-    
 })
 
 /* root route */
-app.get('/', (request, response, next) => {
+app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 /* get all the entries of the database */
-app.get('/api/persons', (request, response) => {
-    Person
+app.get('/api/persons', (request, response, next) => {
+  Person
     .find({})
     .then(persons => {
-        response.json(persons)
+      response.json(persons)
     })
     .catch(error => next(error))
 })
 
 /* info route */
 app.get('/info', (request, response, next) => {
-    Person.find({}).then(result => {
-        let date = new Date()
-        response.send(`<p>Phonebook has info for ${result.length} people</p> <p>${date}</p>`)
+  Person
+    .find({})
+    .then(result => {
+      let date = new Date()
+      response.send(`<p>Phonebook has info for ${result.length} people</p> <p>${date}</p>`)
     })
     .catch(error => next(error))
 })
 
 /* handling get request for individual entries */
 app.get('/api/persons/:id', (request, response, next) => {
-    Person
+  Person
     .findById(request.params.id)
     .then(person => {
-        if(person){
-            response.json(person)
-        }else{
-            response.status(404).end()
-        }
+      if(person){
+        response.json(person)
+      }else{
+        response.status(404).end()
+      }
     })
     .catch(error => next(error))
 })
 
 /* handling delete requests */
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person
-        .findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  Person
+    .findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-    
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    }else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-    next(error)
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
 }
 
 app.use(errorHandler)
--- mongo.js
+++ mongo.js
@@ -17,41 +17,41 @@ const personSchema = new mongoose.Schema({
 const Person = mongoose.model('Person', personSchema)
 
 const addPerson = (fullname, number) => {
-    const person = new Person({
-        name: fullname,
-        number: number,
-    })
-    return person.save()
+  const person = new Person({
+    name: fullname,
+    number: number,
+  })
+  return person.save()
 }
 
 const getAllPeople = () => {
-    return Person.find({})
+  return Person.find({})
 }
 
 mongoose
-    .connect(url)
-    .then((result) => {
-        console.log('connected')
-        if(process.argv.length === 3) {
-            getAllPeople()
-                .then(result => {
-                    console.log('phonebook')
-                    result.forEach(person => {
-                        console.log(`${person.name} ${person.number}`)
-                    })
-                    return mongoose.connection.close()
-                })
-        }else if(process.argv.length === 5){
-            let fullname = process.argv[3]
-            let number = process.argv[4]
-            addPerson(fullname, number)
-                .then((result) => {
-                    console.log(`added ${fullname} number ${number} to phonebook`)
-                    return mongoose.connection.close()
-                })
-        }else{
-            console.log('Invalid number of arguments')
-            process.exit(1)
-        }
-    })
-    .catch((err) => console.log(err))
+  .connect(url)
+  .then(() => {
+    console.log('connected')
+    if(process.argv.length === 3) {
+      getAllPeople()
+        .then(result => {
+          console.log('phonebook')
+          result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+          })
+          return mongoose.connection.close()
+        })
+    }else if(process.argv.length === 5){
+      let fullname = process.argv[3]
+      let number = process.argv[4]
+      addPerson(fullname, number)
+        .then(() => {
+          console.log(`added ${fullname} number ${number} to phonebook`)
+          return mongoose.connection.close()
+        })
+    }else{
+      console.log('Invalid number of arguments')
+      process.exit(1)
+    }
+  })
+  .catch((err) => console.log(err))
